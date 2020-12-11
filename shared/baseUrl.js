1d8b0aba0961cd8e7c0f21802dfd5d0b70e96487
joypoddar <joypoddar.sd@gmail.com>
2020-12-11 16:01:29 +0530

    fix: minor change

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -2,10 +2,8 @@ export const baseURL = "http://192.168.0.104:3001/"; // json-server --watch db.j
 // export const baseURL = "http://192.168.0.104:8000/"; // json-server --watch db.json -d 2000 -p 8000 -H 192.168.0.104
 
 // https://stackoverflow.com/questions/47417766/calling-locally-hosted-server-from-expo-app
-import Constants from "expo-constants";
+// import Constants from "expo-constants";
 
-const { manifest } = Constants;
+// const { manifest } = Constants;
 
-export const api = `http://${manifest.debuggerHost.split(":").shift()}:3001`;
-
-console.log(api);
+// export const api = `http://${manifest.debuggerHost.split(":").shift()}:3001`; // output same as the value of baseURL
